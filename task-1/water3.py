1adb824d2745c846a790b71ce2b3b48bc6c5df3f
Putthiti2738 <144705936+Putthiti2738@users.noreply.github.com>
2023-09-12 19:32:29 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,19 +1,24 @@
 def initial_state():
     return (8, 0, 0)
 
+
 def is_goal(s):
     return s[0] == 4 and s[1] == 4
 
+
+
+
+
 def successors(s):
     x, y, z = s
-    Capacity = (8, 5, 3)
-    Action = []
+    capacities = (8, 5, 3)
+    actions = []
     for i in range(3):
         for j in range(3):
             if i != j:
-                Amount = min(x[i], capacities[j] - x[j])
-                State = list(x)
-                State[i] -= Amount
-                State[j] += Amount
-                Action.append((tuple(State), Amount))
-    return Action
+                amount = min(x[i], capacities[j] - x[j])
+                state = list(x)
+                state[i] -= amount
+                state[j] += amount
+                actions.append((tuple(state), amount))
+    return actions
